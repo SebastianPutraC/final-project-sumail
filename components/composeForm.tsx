377ba0dca952e8698620ac0f449abdd81a1e443a
SebastianPutraC <sebastianputrac@gmail.com>
2025-11-24 20:31:15 +0700

    Send by email

--- components/composeForm.tsx
+++ components/composeForm.tsx
@@ -1,13 +1,13 @@
 'use client'
 
 import firebase from  "../firebase/firebaseConfig"
-import {addDoc, collection, Timestamp, getDoc, doc} from "firebase/firestore";
+import {addDoc, collection, Timestamp, getDoc, doc, getDocs} from "firebase/firestore";
 import React, { useState } from "react";
 import { useRouter } from "next/navigation";
 import {GetCurrentUser} from "@/utils/CurrentUser";
 
 interface ComposeProps {
-    receiverId: string;
+    receiverEmail: string;
     title: string;
     content: string;
 }
@@ -16,9 +16,9 @@ export default function ComposeForm()
 {
     const router = useRouter();
     const [composeForm, setComposeForm] = useState<ComposeProps>(
-        {receiverId: "", title: "", content: ""})
+        {receiverEmail: "", title: "", content: ""})
     const [composeError, setComposeError] = useState<ComposeProps>(
-        {receiverId: "", title: "", content: ""})
+        {receiverEmail: "", title: "", content : ""})
     const {user, loading} = GetCurrentUser();
 
     const [otherError, setOtherError] = useState("Unknown Error");
@@ -33,26 +33,36 @@ export default function ComposeForm()
 
     const validateData = () => {
         const newErrors : ComposeProps =
-            {receiverId: "", title: "", content: ""};
-        if (!composeForm.receiverId) newErrors.receiverId = "Receiver is required";
+            {receiverEmail: "", title: "", content: ""};
+        if (!composeForm.receiverEmail) newErrors.receiverEmail = "Receiver is required";
         if (!composeForm.title) newErrors.title = "Title is required";
-        if (!composeForm.content) newErrors.content = "Content is required";
 
         return newErrors;
     }
 
     const sendMessage = async () => {
         const validationError = validateData();
-        if(validationError.receiverId !== "" && validationError.title !== ""
-            && validationError.content !== "")
+        if(validationError.receiverEmail !== "" && validationError.title !== "")
         {
             setComposeError(validationError);
         }
         else
         {
             try {
-                const receiverRef = doc(firebase.db, "users",
-                    composeForm.receiverId)
+                const userDocs =
+                    await getDocs(collection(firebase.db, "users"));
+                const emailDocs = (userDocs.docs.map((doc) => doc.data().email));
+                const receiverIndex  = emailDocs.
+                    findIndex(receiver => receiver === composeForm.receiverEmail)
+
+                if (receiverIndex === -1) {
+                    setOtherError("No receiver email");
+                    throw new Error(otherError)
+                }
+
+                const receiverId = userDocs.docs[receiverIndex].id;
+
+                const receiverRef = doc(firebase.db, "users", receiverId)
                 const receiverSnap = await getDoc(receiverRef)
                 if (!receiverSnap.exists() || receiverSnap.id === user.id)
                 {
@@ -63,7 +73,7 @@ export default function ComposeForm()
                 const docRef = await addDoc(collection
                 (firebase.db, "messages"), {
                     senderId: user.id,
-                    receiverId: composeForm.receiverId,
+                    receiverId: receiverId,
                     title: composeForm.title,
                     content: composeForm.content,
                     sentDate: Timestamp.fromDate(new Date())
@@ -89,10 +99,10 @@ export default function ComposeForm()
         <div className="listContainer">
             <h2>Compose Message</h2>
             <div>
-                <label>Receiver Id:</label>
-                <input style={{ outlineStyle: 'solid'}} type="text" name="receiverId" onChange={handleChange} />
-                {composeError.receiverId &&(
-                    <div>{composeError.receiverId}</div>
+                <label>Receiver Email:</label>
+                <input style={{ outlineStyle: 'solid'}} type="text" name="receiverEmail" onChange={handleChange} />
+                {composeError.receiverEmail &&(
+                    <div>{composeError.receiverEmail}</div>
                 )}
             </div>
             <div>
@@ -107,9 +117,6 @@ export default function ComposeForm()
                 <label>Content:</label>
                 <textarea style={{ outlineStyle: 'solid', width: '500px', height: '175px'}}
                        name="content" onChange={handleChange} />
-                {composeError.content &&(
-                    <div>{composeError.content}</div>
-                )}
             </div>
             <button onClick={sendMessage}>
                 Send Message
